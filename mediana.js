f43040169682021cdb6c44d20785f903536678f5
Luis Ariza <luismanuelariza@hotmail.com>
2022-01-28 15:30:55 -0500

    Codigo de moda

--- mediana.js
+++ mediana.js
@@ -1,22 +1,23 @@
-//Lista desordenada
+/** Lista de array desordenada */
 const lista1 = [500, 1300, 900, 1500, 700, 1100, 100, 300];
-//Algoritmo de ordenamiento
-for (let i = 0; i < lista1.length - 1; i++) {
-    for (let j = 0; j < lista1.length - 1; j++) {
-        if (lista1[j] > lista1[j + 1]) {
-            const aux = lista1[j];
-            lista1[j] = lista1[j + 1];
-            lista1[j + 1] = aux;
-        }
-    }
-}
+/**
+* Funcion de comparación para ordenación natural
+* https://lenguajejs.com/javascript/fundamentos/arrays/#funci%C3%B3n-de-comparaci%C3%B3n
+* @param {number} a Primer elemento del array
+* @param {number} b Segundo elemento del array
+* @return {number} La función se encargará de, si devuelve 'true' cambia el orden de a y b,
+* si devuelve 'false' los mantiene igual
+*/
+const fc = (a, b) => a - b;
+//Ordenar lista de array con '.sort' mediante la función de comparación 'fc'
+lista1.sort(fc);
 /**
 * Calcular la mediana de la lista de array
 * @param {array} lista 'Lista de array' la cual se va a calcular la mediana
 * @return {number} La mediana de la lista
 */
 const calcularMediana = (lista) => {
-    /** Mitad de la lista, si es numero impar 'parseInt' redondea el numero */
+    /** Mitad de la lista, si el resultado es impar 'parseInt' redondea el numero */
     const mitadLista = parseInt(lista.length / 2);
     //Comprobar si la 'lista' dividida en 2 tiene o no residuo
     if(lista.length % 2 === 0) {
