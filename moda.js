33fa9d71dbe6dff0b2a73709e8da5d27ad8385db
Luis Ariza <luismanuelariza@hotmail.com>
2022-02-03 20:20:22 -0500

    Agregando inputs al promedio

--- moda.js
+++ moda.js
@@ -1,8 +1,8 @@
-const lista = [9, 2, 2, 9, 2, 4, 9, 9, 2, 2, 2, 9, 5, 5, 5, 5, 4, 4];
+const lista = [9, 2, 2, 9, 2, 9, 9, 5, 5, 5, 4, 9, 9, 2, 2, 2, 5, 5, 2, 5, 4, 4];
 
 function calcularModa(arr) {
     /**
-    * Funcion de comparación para ordenación natural, la función se encargará de, si devuelve
+    * Funcion de comparación para ordenación natural, la función se encargará de: si devuelve
     * 'true' cambia el orden de a y b, si devuelve 'false' los mantiene igual
     * https://lenguajejs.com/javascript/fundamentos/arrays/#funci%C3%B3n-de-comparaci%C3%B3n
     * @param {number} a Primer elemento del array filtrado y tomando el tamaño de cuantos elemento hay
@@ -10,7 +10,7 @@ function calcularModa(arr) {
     * @return {number} la comparación de los 2 array-elementos
     */
     const fc = (a, b) => arr.filter(v => v === a).length - arr.filter(v => v === b).length;
-
+    
     return arr.sort(fc).pop();
 }
 
--- promedio.js
+++ promedio.js
@@ -1,10 +1,35 @@
-const lista1 = [200, 100, 500, 400, 300];
+/**
+* Procesar los "inputs" del archivo HTML y convertir a 'Number' el tipo de dato del valor de entrada
+* @param {string} medidaFigura 'Nombre de la medida y figura' que se desea obtener
+* @return {number} EL valor de la medida ingresada
+*/
+function getInput(medidaFigura) {
+    /** Nombre de ID's de los elementos "input" de HTML, guardadas en las propiedades del objeto */
+    const input = {
+        datoPromedio: "inputDatos"
+    };
+    /** Dato de 'input[]' obtenido por el parámetro 'medidaFigura' */
+    const datoInput = document.getElementById(input[medidaFigura]);
+    /** Valor de 'datoInput' con decimales, convertido de tipo de dato 'String' a 'Number' */
+    const valorInputFiltrado = Number.parseFloat(datoInput.value);
+
+    return valorInputFiltrado;
+}
+function ingresarDato() {
+    const dato = getInput('datoPromedio');
+    const lista = [];
+    
+    lista.push(dato);
+
+    return alert(lista);
+}
 /**
 * Calcular el promedio de la lista de array
 * @param {array} lista 'Lista de array' la cual se va a calcular el promedio
 * @return {number} El promedio de la lista, la suma de la lista sobre el tamaño de la lista
 */
-const calcularPromedio = (lista) => {
+const calcularPromedio = () => {
+    const lista = ingresarDato();
     /**
     * Funcion para 'reduce', sumar todos los elementos array
     * @param {number} acumulador numero acumulado o sumado, inicia en la posición 0 del array
@@ -18,5 +43,5 @@ const calcularPromedio = (lista) => {
     */
     const sumaLista = lista.length > 0 ? lista.reduce(acumular) : 0;
 
-    return sumaLista / lista.length;
+    return alert(sumaLista / lista.length);
 }
