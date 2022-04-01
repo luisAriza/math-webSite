8d7c2f40850a09825ae0e51127a06eebf5b61bc3
Luis Ariza <luismanuelariza@hotmail.com>
2022-03-31 20:41:13 -0500

    Loader de Pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/js/index.js",
   output: {
-    path: __dirname + "/build",
+    path: path.join(__dirname, 'dist/'),
     filename: "bundle.js",
   },
   devServer: {
     port: 5000,
   },
-  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src/views/index.pug"),
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.pug$/,
+        loader: "@webdiscus/pug-loader",
+      },
+    ],
+  },
 };
